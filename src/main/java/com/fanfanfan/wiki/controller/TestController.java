7597ce203bd9ffdfaef26e83bc960fe55cdbbc15
fanfan <1078189582@qq.com>
2022-08-13 20:23:17 +0800

    制作自定义组件

---
+++ src/main/java/com/fanfanfan/wiki/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.fanfanfan.wiki.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello111";
+    }
+    @PostMapping("/hello/post")
+    public String hepost(String name){
+        return "helloworld"+name;
+    }
+
+}
