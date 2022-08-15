4f5cfa33cea9d9f77e33919a47efee2e2084afc8
fanfan <1078189582@qq.com>
2022-08-15 22:13:56 +0800

    9.Vue多环境配置

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,4 +4,5 @@ import router from './router'
 import store from './store'
 import Antd  from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-createApp(App).use(store).use(router).use(Antd).mount('#app')
+createApp(App).use(store).use(router).use(Antd).mount('#app');
+console.log('环境',process.env.NODE_ENV);
