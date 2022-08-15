23215677a64e2e70df5c277414421b37c8e9bf63
fanfan <1078189582@qq.com>
2022-08-15 22:51:45 +0800

    14.增加拦截器打印接口耗时

---
+++ src/main/java/com/fanfanfan/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,42 @@
+package com.fanfanfan.wiki.config;
+
+//import com.fanfanfan.wiki.interceptor.ActionInterceptor;
+import com.fanfanfan.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor loginInterceptor;
+
+ /*   @Resource
+    ActionInterceptor actionInterceptor;
+*/
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(//不拦截的请求
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
+
+    //  registry.addInterceptor(actionInterceptor)
+      //          .addPathPatterns(
+        //                "/*/save",
+          //              "/*/delete/**",
+            //            "/*/reset-password");
+
+    }
+}
