b0ceb341da6b4680dbdafa2e10b8f7e7cacbdbad
fanfan <1078189582@qq.com>
2022-08-16 15:50:34 +0800

    19.增加电子书管理页面

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -4,10 +4,118 @@
                 :style="{ background: '#D8BFD8', padding: '24px', margin: 0, minHeight: '280px' }"
         >
             <!--列表内容-->
-            <div class="about">
-                <h2>电子书管理</h2>
-            </div>
-
+            <a-table
+                    :columns="columns"
+                    :row-key="record => record.id"
+                    :data-source="ebooks"
+                    :pagination="pagination"
+                    :loading="loading"
+                    @change="handleTableChange"
+            >
+                <template #cover="{ text: cover }">
+                    <img v-if="cover" :src="cover" alt="avatar" />
+                </template>
+                <template v-slot:action="{ text, record }">
+                    <a-space size="small">
+                        <a-button type="primary">
+                            编辑
+                        </a-button>
+                        <a-button type="danger">
+                                删除
+                        </a-button>
+                    </a-space>
+                </template>
+            </a-table>
         </a-layout-content>
     </a-layout>
 </template>
+<script lang="ts">
+   // import { SmileOutlined, DownOutlined } from '@ant-design/icons-vue';
+    import { defineComponent ,ref,onMounted} from 'vue';
+    import axios from 'axios';
+
+    export default defineComponent({
+        name:'AdminEbook',
+        components: {
+        },
+        setup() {
+            const param = ref();
+            param.value = {};
+            const ebooks = ref();
+            const pagination = ref({
+                current: 1,
+                pageSize: 10,
+                total: 0
+            });
+            const loading = ref(false);
+            const columns = [
+                {
+                    title: '封面',
+                    dataIndex: 'cover',
+                    slots: { customRender: 'cover' }
+                },
+                {
+                    title: '名称',
+                    dataIndex: 'name'
+                },
+                {
+                    title: '分类',
+                    slots: { customRender: 'category' }
+                },
+                {
+                    title: '文档数',
+                    dataIndex: 'docCount'
+                },
+                {
+                    title: '阅读数',
+                    dataIndex: 'viewCount'
+                },
+                {
+                    title: '点赞数',
+                    dataIndex: 'voteCount'
+                },
+                {
+                    title: 'Action',
+                    key: 'action',
+                    slots: { customRender: 'action' }
+                }
+            ];
+            const handleQuery=(params: any)=>{
+                loading.value=true;
+                axios.get("/ebook/list",params).then(
+                    (response)=>{
+                        loading.value=false;
+                        const data=response.data;
+                        ebooks.value=data.content;
+                        pagination.value.current=params.page;
+                    });
+            };
+            /**
+             * 表格点击页码时触发
+             */
+            const handleTableChange = (pagination: any) => {
+                console.log("看看自带的分页参数都有啥：" + pagination);
+                handleQuery({
+                    page: pagination.current,
+                    size: pagination.pageSize
+                });
+            };
+            onMounted(()=>{
+                handleQuery({});
+                });
+            return {
+                columns,
+                ebooks,
+                pagination,
+                loading,
+                handleTableChange
+            };
+        },
+    });
+</script>
+
+<style scoped>
+    img {
+        width: 50px;
+        height: 50px;}
+</style>
